84a6f401190eab512b24d8830f276055c1d86120
Leyre Osés Sánchez <48771457+leoses@users.noreply.github.com>
2021-03-25 12:31:45 +0100

    Agregadas plantilla de Fede, ahora teseo y el minotauro se crean a traves del mapa
    Co-Authored-By: Amaro Blest Polo <36236348+Sounagix@users.noreply.github.com>
    Co-Authored-By: Stiven Arias Giraldo <47497948+StAincrad@users.noreply.github.com>
    Co-Authored-By: auroraGJ <48923631+auroraGJ@users.noreply.github.com>

---
+++ Assets/Scripts/MinotaurMovable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class MinotaurMovable : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector3 target;
+    private Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
