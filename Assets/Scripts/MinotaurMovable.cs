28dc11eb662a3cd0626a9c7d8e2a982f7ab9854c
Leyre Osés Sánchez <48771457+leoses@users.noreply.github.com>
2021-03-25 14:45:21 +0100

    ADDED: Merodeo del minotauro casi completado
    Co-Authored-By: Amaro Blest Polo <36236348+Sounagix@users.noreply.github.com>
    Co-Authored-By: Stiven Arias Giraldo <47497948+StAincrad@users.noreply.github.com>
    Co-Authored-By: auroraGJ <48923631+auroraGJ@users.noreply.github.com>
    Co-Authored-By: pamart24 <48999523+pamart24@users.noreply.github.com>

--- Assets/Scripts/MinotaurMovable.cs
+++ Assets/Scripts/MinotaurMovable.cs
@@ -9,21 +9,35 @@ public class MinotaurMovable : MonoBehaviour
     private Rigidbody rb;
     private Vector3 target;
     private Vector3 velocity;
-
+    private Vector3 bound;
+    public float force = 10;
+    public float raycastDistance = 2.0f;
+    private const int minotaurLayer = 9;
+    public Transform raycastStart;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        velocity = new Vector3(1, 0, 0);
     }
 
     private void FixedUpdate()
     {
-        
+        RaycastHit hit;
+        if (Physics.Raycast(raycastStart.position, transform.forward,out hit, raycastDistance))
+        {
+            if (hit.transform.gameObject.tag == "Wall")
+            {
+                Debug.DrawLine(transform.position, hit.point, Color.green);
+                rb.velocity = Vector3.zero;
+
+                if (transform.forward == Vector3.right) transform.forward = Vector3.back;
+                else if (transform.forward == Vector3.back) transform.forward = Vector3.left;
+                else if (transform.forward == Vector3.left) transform.forward = Vector3.forward;
+                else transform.forward = transform.right;
+            }
+        }
+
+        rb.AddForce(transform.forward * force, ForceMode.Force);
     }
 }
